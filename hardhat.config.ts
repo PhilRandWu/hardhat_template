2cd4f1ac8481a456201aea9d56468d3a0e154761
wenbo <3378814052@qq.com>
2024-03-19 11:04:23 +0800

    init project

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,29 @@
-import { HardhatUserConfig } from "hardhat/config";
+import {HardhatUserConfig} from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-deploy";
+import 'dotenv/config';
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
-  defaultNetwork: "hardhat",
+    solidity: {
+        compilers: [
+            {version: "0.8.24"}
+        ]
+    },
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+        },
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+            1: 0
+        },
+        player: {
+            default: 1
+        },
+    }
 };
 
 export default config;
